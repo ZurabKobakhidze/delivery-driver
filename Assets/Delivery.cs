c8bfe66d332ae800e9746b0d7540479238856f23
ZurabKobakhidze <zukakobakhidze@gmail.com>
2023-06-18 22:49:23 +0400

    final touch

--- Assets/Delivery.cs
+++ Assets/Delivery.cs
@@ -4,6 +4,19 @@ using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
+
+    [SerializeField] Color32 hasPackageColor =  new Color32 (169, 1, 1, 1);
+    [SerializeField] Color32 noPackageColor =  new Color32 (1, 1, 1, 1);
+    [SerializeField] float destroyDelay = 0.5f;
+    bool hasPackage;
+
+    SpriteRenderer spriteRenderer;
+
+    void Start() 
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
     void OnCollisionEnter2D(Collision2D other)
     {
         Debug.Log("Ouch!");
@@ -12,14 +25,19 @@ public class Delivery : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.tag == "Package")
+        if (other.tag == "Package" && !hasPackage)
         {
             Debug.Log("Package picked up");
+            hasPackage = true;
+            spriteRenderer.color = hasPackageColor;
+            Destroy(other.gameObject, destroyDelay);
         }
 
-        if (other.tag == "Customer")
+        if (other.tag == "Customer" && hasPackage)
         {
             Debug.Log("Package Delivered");
+            hasPackage = false;
+            spriteRenderer.color = noPackageColor;
         }
 
     }
